b10c370fc8a85b1d4d790cf93864933b27121f9c
AntzyMo <mozbano@163.com>
2023-06-07 14:56:52 +0800

    fix: add os EOL

--- src/helper.js
+++ src/helper.js
@@ -1,4 +1,6 @@
+import os from 'node:os'
+
 // 解析行尾符是 \r\n -> CRLF 还是 \n -> LF
 export function resolveEndOfLine(sourceCode) {
-  return sourceCode.match(/(\r\n|\n){1}/g)[0]
+  return sourceCode.match(/(\r\n|\n){1}/g)?.[0] || os.EOL
 }
